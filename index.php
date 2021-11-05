f8f8c96af70ccd82223895e714706c7b972cc225
HernandezJuarezDaniel <91238515+HernandezJuarezDaniel@users.noreply.github.com>
2021-11-04 21:24:53 -0600

    Add files via upload

---
+++ index.php
@@ -0,0 +1,66 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+    <title>5i 18 Hernandez Juarez Daniel</title>
+    
+	<link rel="stylesheet" href="styles.css">
+</head>
+<body>
+	<header>
+		<div class="wrapp">
+			<div class="logo">
+				<a href="#"><img src="img/logo.png" alt="logofut"></a>
+			</div>
+			<nav>
+				<ul>
+					<li><a href="index.php">Inicio</a></li>
+					<li><a href="personal.php">Personal</a></li>
+					<li><a href="jugador.php">Jugadores</a></li>
+				</ul>
+			</nav>
+		</div>
+	</header>
+	<section class="main">
+		<div class="wrapp">
+			<div class="mensaje">
+				<h1>Bienvenidos! Sitio web de nuestro Club de Futbol</h1>
+			</div>
+
+			<div class="articulo">
+				<article>
+					<p>Descripcion de mi empresa</p>
+					<br/>
+					<p> Continua Descripcion  </p>
+					<br/>
+					<p>Continua Descripcion</p>
+					<br/>
+					<p>Continua Descripcion</p>
+				</article>
+			</div>
+<br><br>
+			<aside>
+				<div class="widget">
+					<h3>Proximos Juegos:</h3>
+					<ul>
+						<li><a href="#">Juego 1</a></li>
+						<li><a href="#">Juego 2</a></li>
+						<li><a href="#">Juego 3</a></li>
+						<li><a href="#">Juego 4</a></li>
+						<li><a href="#">Juego 5</a></li>
+					</ul>
+				</div>
+			</aside>
+		</div>
+	</section>
+	<br>
+</body>
+<footer>
+		<div class="wrapp">
+            <p> No lista 18 </p>
+            <p> // SUBMÓDULO 2
+                Desarrolla aplicaciones Web con conexión a bases de datos</p>
+            <p>https://github.com/HernandezJuarezDaniel</p>
+		</div>
+	</footer>
+</html>
