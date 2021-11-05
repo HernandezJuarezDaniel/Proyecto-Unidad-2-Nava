71e7e0340809582ce2b85b65778de88344b2816e
HernandezJuarezDaniel <91238515+HernandezJuarezDaniel@users.noreply.github.com>
2021-11-05 13:36:28 -0600

    Add files via upload

---
+++ personal.php
@@ -0,0 +1,78 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+    <title>5i 18 Hernandez Juarez Daniel</title>
+    
+	<link rel="stylesheet" href="styles.css">
+</head>
+<body>
+	<header>
+		<div class="wrapp">
+			<div class="logo">
+				<a href="#"><img src="img/logo.png" alt="logofut"></a>
+			</div>
+			<nav>
+				<ul>
+					<li><a href="index.php">Inicio</a></li>
+					<li><a href="personal.php">Personal</a></li>
+					<li><a href="jugador.php">Jugadores</a></li>
+				</ul>
+			</nav>
+		</div>
+	</header>
+	<section class="main">
+		<div class="wrapp">
+			<div class="mensaje">
+				<h1>Bienvenidos! Sitio web de nuestro Club de Futbol</h1>
+			</div>
+			<div class="mensaje2">
+				<h1>Personal</h1>
+			</div>
+
+
+<div class="articulo">
+  <form>
+    <label for="fname">Nombre Del Empleado:</label>
+    <input type="text" id="fname" name="firstname" placeholder="Nombre">
+	<br><br>
+
+    <label for="lname">Apellido del Empleado:</label>
+    <input type="text" id="lname" name="lastname" placeholder="Apellido">
+<br><br>
+    <label for="equipo">Equipo que Lidera:</label>
+    <select id="equipo" name="equipo">
+      <option value="cbtis">CBTIS128 FC</option>
+      <option value="barcelona">Barcelona</option>
+      <option value="chivas">Las Chivas</option>
+    </select>
+<br>  
+    <input type="submit" value="Ingresar">
+  </form>	
+</div>
+<aside>
+				<div class="widget">
+					<h3>Proximos Juegos:</h3>
+					<ul>
+						<li><a href="#">Juego 1</a></li>
+						<li><a href="#">Juego 2</a></li>
+						<li><a href="#">Juego 3</a></li>
+						<li><a href="#">Juego 4</a></li>
+						<li><a href="#">Juego 5</a></li>
+					</ul>
+				</div>
+			</aside>
+		</div>
+	</section>
+	
+	<br>
+</body>
+<footer>
+		<div class="wrapp">
+            <p> No lista 18 </p>
+            <p> // SUBMÓDULO 2
+                Desarrolla aplicaciones Web con conexión a bases de datos</p>
+            <p>https://github.com/HernandezJuarezDaniel</p>
+		</div>
+	</footer>
+</html>
